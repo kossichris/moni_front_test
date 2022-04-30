6fdd5624a7e29ce00bc3ffae74465309e251cb43
chris <chrishouns21@gmail>
2022-04-30 11:02:32 -0400

    auth login

--- src/auth/authProvider.js
+++ src/auth/authProvider.js
@@ -4,5 +4,5 @@ import { Navigate, Outlet } from "react-router-dom";
 export default function PrivateRoute(props) {
   // If authorized, return an outlet that will render child elements
   // If not, return element that will navigate to login page
-  return props.auth ? <Outlet /> : <Navigate to="/signup" />;
+  return props.auth ? <Outlet /> : <Navigate to="/login" />;
 }
